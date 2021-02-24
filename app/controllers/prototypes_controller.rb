3ed07683547b4231cf99cd9b055d215288776b40
morioka1206 <r.morioka@icloud.com>
2021-02-24 15:10:36 +0900

    fifth commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]  
+  before_action :authenticate_user!, except: [:index, :show, :create]  
   before_action :move_to_index, except: [:index, :show, :new, :create]
 
   def index
